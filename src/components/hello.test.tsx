6f086b641bd23fcf63a67bffd8c02b8ffdea0c9a
farmerc <cameron.farmer@umusic.com>
2019-02-18 11:23:02 -0600

    add Hello component test

--- src/components/hello.test.tsx
+++ src/components/hello.test.tsx
@@ -1,11 +1,24 @@
+//https://testing-library.com/docs/react-testing-library/intro
+//Import Dependencies//
 import * as React from 'react'
-import { render }  from 'react-testing-library'
+import { Hello, HelloProps } from './Hello'
+
+// import react-testing methods
+import { render, fireEvent, cleanup }  from 'react-testing-library'
+
+// add custom jest matchers from jest-dom
+//https://www.npmjs.com/package/jest-dom
 import 'jest-dom/extend-expect'
+//END Dependencies//
 
-const Hello = () => <h1>Hello World</h1>;
+//Start test//
 
-test('first hello test', () => {
-    const { container } = render(<Hello />);
+afterEach(cleanup); // automatically unmount and cleanup DOM after the test is finished.
 
-    expect(container).toHaveTextContent('Hello World')
+test('should contain user greeting', () => {
+    const {
+        getByText, //will search for all elements that have a text node with textContent matching the given TextMatch
+    } = render(<Hello firstName="First" lastName="Last"/>); //Render into a container which is appended to document.body
+
+    getByText("Hello from First Last");
 });
